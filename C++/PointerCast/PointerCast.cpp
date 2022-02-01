b1a79dd46a8a40b56b9f9a540b9c585033e830fc
arad4228 <arad4228@gmail.com>
2022-02-01 15:29:21 +0900

    포인터 연산 공부업로드

---
+++ C++/PointerCast/PointerCast.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int arr[3] = {10, 20, 30};	
+	int* ptr_arr = arr;			
+	
+	cout << "배열의 이름을 이용하여 배열 요소에 접근 : " << arr[0] << ", " << arr[1] << ", "  << arr[2] << endl;
+
+    // C언어와 같이 증감 연산자를 통해 접근이 가능하다.
+	cout << "    포인터를 이용하여 배열 요소에 접근 : " << *(ptr_arr) << ", " << *(ptr_arr+=1) << ", "  << *(ptr_arr+=1) << endl;
+    // 또는 배열 처럼 취급해서 접근이 가능하다.
+	cout << " 포인터를 이용하여 배열 요소에 접근 : " << ptr_arr[0] << ", " << ptr_arr[1] << ", " << ptr_arr[2] << endl;
+	cout << "배열의 이름을 이용한 배열의 크기 계산 : " << sizeof(arr) << endl;
+	cout << "    포인터를 이용한 배열의 크기 계산 : " << sizeof(ptr_arr)<< endl;
+
+    return 0;
+}
