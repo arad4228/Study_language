c2fe6055934af48ceb32dc7812836eaed8c59bd4
arad4228 <arad4228@gmail.com>
2022-02-02 18:33:28 +0900

    C언어 스타일 문자열 공부 업로드(수정)

--- C++/.history/C'sString/NonString_20220202183014.cpp
+++ C++/.history/C'sString/NonString_20220202183014.cpp
@@ -8,27 +8,25 @@ int main(void)
     const int SIZE = 20;
     
     // C언어는 문자열을 만들기 위해 char배열을 사용했었다.
-    char address1[SIZE];
-    char address2[SIZE];
-    char name1[SIZE];
-    char name2[SIZE];
+    char address[SIZE];
+    char name[SIZE];
     
     cout << "자신의 이름을 적어주세요 : ";
     // printf("%s", name);과 같은 역활을 하는 것으로 추정.
     // 실제는 getc와 같은 역활을 하며 객체지향의 특성으로 인자로 값을 줄 수 있음.
-    cin.get(name1, SIZE).get();
+    cin.get(name, SIZE).get();
     cout << "자신이 살고 있는 도시를 적어주세요 : ";
-    cin.get(address1, SIZE).get();
+    cin.get(address, SIZE).get();
 
-    cout << "안녕하세요" <<address1 << "에 살고 있는 " << name1 << "입니다. \n감사합니다!"<< endl;
+    cout << "안녕하세요" <<address << "에 살고 있는 " << name << "입니다. \n감사합니다!"<< endl;
 
     // 20바이트를 넘어도 입력은 받지만 20바이트까지만 저장할 수 있게하는 방법.
     cout << "자신의 이름을 적어주세요 : ";
-    cin.get(name2, SIZE).ignore(SIZE, '\n');
+    cin.get(name, SIZE).ignore(SIZE, '\n');
     cout << "자신이 살고 있는 도시를 적어주세요 : ";
-    cin.get(address2, SIZE).ignore(SIZE, '\n');
+    cin.get(address, SIZE).ignore(SIZE, '\n');
     
-    cout << "안녕하세요" <<address2 << "에 살고 있는 " << name2 << "입니다. \n감사합니다!"<< endl;
+    cout << "안녕하세요" <<address << "에 살고 있는 " << name << "입니다. \n감사합니다!"<< endl;
 
     return 0;
 }
