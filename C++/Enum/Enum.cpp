6e50058cb207fa86fead072176bc96ab69904988
arad4228 <arad4228@gmail.com>
2022-02-03 15:45:13 +0900

    Enum 관련 공부 업로드

---
+++ C++/Enum/Enum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+enum Weather {Sunny = 0, Cloudly = 10, Rain = 20, Snow = 30};
+
+int main(void)
+{
+    int input;
+    Weather wt;
+    cout << "Sunny = 0, Cloudly = 10, Rain = 20, Snow = 30}" << endl;
+    cin >> input;
+    wt = (Weather)input;
+
+    switch (wt)
+    {
+    case Sunny:
+        cout << "오늘의 날씨는 맑음입니다. "  << endl;
+        break;
+    case Cloudly:
+        cout << "오늘의 날씨는 흐림입니다. "  << endl;
+        break;
+    case Rain:
+        cout << "오늘의 날씨는 비옴입니다. "  << endl;
+        break;
+    case Snow:
+        cout << "오늘의 날씨는 눈옴입니다. "  << endl;
+        break;
+    default:
+        cout << "정확한 값을 입력하세요 " << endl;
+        break;
+    }
+    cout << endl << "Enum Weather의 각 상숫값은 " << Sunny << ", " << Cloudly<< ", "<< Rain << ", " << Snow << "입니다.";
+
+    return 0;
+}
