c044db9206134ea4b440163f0e1b0556b0da79dd
arad4228 <arad4228@gmail.com>
2022-02-04 10:57:39 +0900

    Inline관련 공부 업로드

---
+++ C++/.history/InlineFuntion/Inline_20220204105651.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+inline int sub(int x, int y){return x-y;};
+inline void print(int x){cout << "계산결과는 " << x << "입니다." <<endl;};
+
+int main(void)
+{
+    int num1 = 5;
+    int num2 = 3;
+    int result = sub(num1,num2);
+    print(result);
+    return 0;
+}
