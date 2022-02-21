c6b0ee11e3485b2e9ba22b273e6bcb4f1df22144
arad4228 <arad4228@gmail.com>
2022-02-21 16:20:06 +0900

    Class 관련 공부 업로드

---
+++ C++/Class/Class.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class cat
+{
+    public:
+    string name;
+    string family;
+    int age;
+    double weight;
+
+    cat(string name, string family, int age, double weight)
+    {
+        this->name = name;
+        this->family = family;
+        this->age = age;
+        this->weight = weight;
+    }
+
+    void Mew()
+    {
+        cout << "Mew!" << endl;
+    }
+
+    void Eat()
+    {
+        this->weight++;
+    }
+
+    void Sleep()
+    {
+        cout << "zzzzzz" << endl;
+    }
+    
+    void Play()
+    {
+        cout << "Play with me!" << endl;
+    }
+};
+
+int main(void)
+{
+    cat* c1 = new cat("나비", "코리안 숏 헤어", 1, 0.1);
+    cout << "고양이의 정보는 "<< c1->name << " " << c1->family << " " <<c1->age << " ";
+    cout.precision(4);
+    cout << fixed <<c1->weight << endl;
+    c1->Mew();
+    c1->Sleep();
+    c1->Play();
+    
+    return 0;
+}
