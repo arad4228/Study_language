c3792e8cb6b7c34ac9c5fdec735d712c16950e79
arad4228 <arad4228@gmail.com>
2022-03-04 10:21:01 +0900

    이전은 Search, 이번은dictionary Add를 실습

--- Go/Project2/mydict/mydict.go
+++ Go/Project2/mydict/mydict.go
@@ -3,6 +3,7 @@ package mydict
 import "errors"
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
 
 type Dictionary map[string]string
 
@@ -13,4 +14,24 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	/*
+		if err == errNotFound {
+			d[word] = def
+		} else if err == nil {
+			return errWordExists
+		}
+	*/
+	//또는 switch로 가능하다.
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }
