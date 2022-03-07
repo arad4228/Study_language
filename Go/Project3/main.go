3af45f0043ce03fba328b86859b995406d029dad
arad4228 <arad4228@gmail.com>
2022-03-07 14:32:28 +0900

    URL Checker(Fast) Done

--- Go/Project3/main.go
+++ Go/Project3/main.go
@@ -6,13 +6,19 @@ import (
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
 	// 초기화 되지 않은 Map을 사용하기 위해 2방법을 사용
 	//var results = map[string]string{}
 	// 또는 아래의 방법을 사용
-	var results = make(map[string]string)
+	results := make(map[string]string)
+	channel := make(chan requestResult)
 
 	urls := []string{
 		"https://www.airbnb.com/",
@@ -27,24 +33,27 @@ func main() {
 	}
 
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, channel)
 	}
-	for url, result := range results {
-		fmt.Println(url, result)
+
+	// 9개의 메시지를 받는 방법
+	for i := 0; i < len(urls); i++ {
+		result := <-channel
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func hitURL(url string) error {
+// chan<- == send only
+func hitURL(url string, channel chan<- requestResult) {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
+	channel <- requestResult{url: url, status: status}
 }
