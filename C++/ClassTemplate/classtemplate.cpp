aeef5ab2b67ee12be255de871518921702240897
arad4228 <arad4228@gmail.com>
2022-03-18 12:22:05 +0900

    중첩 클래스 템플릿 관련 추가

--- C++/ClassTemplate/classtemplate.cpp
+++ C++/ClassTemplate/classtemplate.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+
+// 해당 클래스는 단일 클래스 템플릿이다.
 template <typename T>
 class Data
 {
@@ -17,7 +19,22 @@ public:
         return this->data;
     }
 };
+/*
+해당 부분은 중첩 클래스 템플릿의 예시이다.
+template <typename T>
+class X
+{
+    template <typename U>
+    class Y
+    {
 
+    }
+}
+
+template <typename T>
+template <typename U>
+X<T>::Y<U>::멤버함수이름(){};
+*/
 int main(void)
 {
     Data<string> str_data("C++ 공부");
