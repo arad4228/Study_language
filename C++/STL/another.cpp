973bd7d582d211b6d22eb0de7e812d4b2bae3b66
arad4228 <arad4228@gmail.com>
2022-03-21 20:03:54 +0900

    Insertion Iterator관련 공부내용 업로드

--- C++/STL/another.cpp
+++ C++/STL/another.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <list>
 using namespace std;
 
 int main(void)
 {
+    // Stream itorator
     vector<int> v = { 1, 2, 3, 4, 5};
     copy(v.begin(), v.end(), ostream_iterator<int>(cout));
     cout << endl;
     copy(v.begin(), v.end(), ostream_iterator<int>(cout," "));
+    cout << endl;
+
+    // Insertion iterator
+    list<int> ls = {10};
+	ls.push_back(20);	// back_insert_iterator를 사용함. 
+	ls.push_front(30);	// front_insert_iterator를 사용함. 
+	copy(ls.begin(), ls.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
     return 0;
 }
