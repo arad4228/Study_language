f3b3598d4bc64abe959014588b84d64c598c7699
arad4228 <arad4228@gmail.com>
2022-03-21 20:10:31 +0900

    Insertion 관련 추가 내용, Reverse iterator관련 내용 업로드

--- C++/STL/another.cpp
+++ C++/STL/another.cpp
@@ -15,10 +15,21 @@ int main(void)
 
     // Insertion iterator
     list<int> ls = {10};
+    /*
+        STL에서 제공하는 삽입 반복자.
+        1. insertion_iterator
+        2. back_insertion_iterator
+        3. front_insertion_iterator 
+    */
 	ls.push_back(20);	// back_insert_iterator를 사용함. 
 	ls.push_front(30);	// front_insert_iterator를 사용함. 
 	copy(ls.begin(), ls.end(), ostream_iterator<int>(cout, " "));
     cout << endl;
 
+    // Reverse iterator
+    list<int> Rs = {10, 20, 30};
+	copy(Rs.rbegin(), Rs.rend(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+    
     return 0;
 }
